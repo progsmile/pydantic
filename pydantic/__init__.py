cb7afeb397295498100e6d3725c9bf741edaeb5d
Marcelo Trylesinski <marcelotryle@gmail.com>
2023-07-18 16:02:11 +0200

    Support pydantic entry point

--- pydantic/__init__.py
+++ pydantic/__init__.py
@@ -38,6 +38,7 @@ from .functional_validators import (
 from .json_schema import WithJsonSchema
 from .main import *
 from .networks import *
+from .plugin import PluginManager
 from .type_adapter import TypeAdapter
 from .types import *
 from .validate_call import validate_call
@@ -45,6 +46,9 @@ from .version import VERSION
 from .warnings import *
 
 __version__ = VERSION
+
+
+PluginManager().load_plugins()
 
 # this encourages pycharm to import `ValidationError` from here, not pydantic_core
 ValidationError = pydantic_core.ValidationError
